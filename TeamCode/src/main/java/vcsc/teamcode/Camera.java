d872e284e5ee33a8e33c639b1873422e0066f4f6
Aiden <aidenrank6@gmail.com>
2024-10-14 20:06:00 -0700

    reorganized things and made place for hardware mapping

--- TeamCode/src/main/java/vcsc/teamcode/Camera.java
+++ TeamCode/src/main/java/vcsc/teamcode/Camera.java
@@ -1,5 +1,30 @@
 package vcsc.teamcode;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
+
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.LLResultTypes;
+import com.qualcomm.hardware.limelightvision.LLStatus;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
 
 public class Camera {
 
+    Limelight3A limelight;
+    public void init() {
+        limelight = hardwareMap.get(Limelight3A.class, "limelight");
+        limelight.setPollRateHz(100); // This sets how often we ask Limelight for data (100 times per second)
+        limelight.start(); // This tells Limelight to start looking!
+        LLResult result = limelight.getLatestResult();
+        if (result != null && result.isValid()) {
+            double tx = result.getTx(); // How far left or right the target is (degrees)
+            double ty = result.getTy(); // How far up or down the target is (degrees)
+            double ta = result.getTa(); // How big the target looks (0%-100% of the image)
+
+            telemetry.addData("Target X", tx);
+            telemetry.addData("Target Y", ty);
+            telemetry.addData("Target Area", ta);
+        } else {
+            telemetry.addData("Limelight", "No Targets");
+        }
+    }
 }
--- TeamCode/src/main/java/vcsc/teamcode/Claw.java
+++ TeamCode/src/main/java/vcsc/teamcode/Claw.java
@@ -1,11 +1,23 @@
 package vcsc.teamcode;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad1;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.ServoImplEx;
 
 public class Claw {
-    public void open() {
+    ServoImplEx servo;
+    static double openPosition = 0.5;
+    static double closedPosition = 0.1;
 
+    public Claw(ServoImplEx servo) {
+        this.servo = servo;
     }
-
+    public void open() {
+        servo.setPosition(openPosition);
+    }
     public void close() {
-
+        servo.setPosition(closedPosition);
     }
 }
