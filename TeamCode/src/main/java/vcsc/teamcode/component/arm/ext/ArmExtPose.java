ccec296964ae7c094107d559d4521412ada1a080
Dallin Guisti <dallin@guisti.org>
2025-02-03 19:56:46 -0800

    Change extension limits

--- TeamCode/src/main/java/vcsc/teamcode/component/arm/ext/ArmExtPose.java
+++ TeamCode/src/main/java/vcsc/teamcode/component/arm/ext/ArmExtPose.java
@@ -2,12 +2,12 @@ package vcsc.teamcode.component.arm.ext;
 
 public enum ArmExtPose {
     RETRACT(0),
-    BASKET(70), //61
+    BASKET(72), //61
     MAX_ROTATE(5),
     INTAKE(40),
     INTAKE_AUTO(30),
     SPECIMEN_PRE_SCORE(9),
-    SPECIMEN_SCORE(30.5),
+    SPECIMEN_SCORE(31.5),
     HANG(18);
 
 
